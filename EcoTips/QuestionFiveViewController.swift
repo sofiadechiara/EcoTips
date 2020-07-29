2064837ed30d6179872ff04418d0c7be515a4ea7
Sofia De Chiara <sofdechiara@gmail.com>
2020-07-29 10:32:55 -0400

    First commit

---
+++ EcoTips/QuestionFiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuestionFiveViewController.swift
+//  EcoTips
+//
+//  Created by Sofia De Chiara on 7/29/20.
+//  Copyright © 2020 Sofia De Chiara. All rights reserved.
+//
+
+import UIKit
+
+class QuestionFiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EcoTips/QuestionOneViewController.swift
@@ -0,0 +1,46 @@
+//
+//  QuestionOneViewController.swift
+//  EcoTips
+//
+//  Created by Sofia De Chiara on 7/29/20.
+//  Copyright © 2020 Sofia De Chiara. All rights reserved.
+//
+
+import UIKit
+
+class QuestionOneViewController: UIViewController {
+
+    @IBOutlet weak var questionOneLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func veganButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func vegetarianButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func pescetarianButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func meatButton(_ sender: UIButton) {
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
