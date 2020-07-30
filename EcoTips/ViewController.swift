14654cc7ea869aeea3e22e20d50230fa0615fa12
Sofia De Chiara <sofdechiara@gmail.com>
2020-07-30 14:28:17 -0400

    Commit 6

---
+++ EcoTips/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  EcoTips
+//
+//  Created by Sofia De Chiara on 7/29/20.
+//  Copyright © 2020 Sofia De Chiara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var carbonFootPrintText: UITextView!
+    @IBOutlet weak var ecoTipsBlurbText: UITextView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
